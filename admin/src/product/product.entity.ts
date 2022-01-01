815a15b029a5882d817d2a281339235cfdc7510b
Rakesh D R <rakesh@rapidinnovation.dev>
2022-01-01 18:25:22 +0530

    Initialized microservices repo

---
+++ admin/src/product/product.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  image: string;
+
+  @Column({ default: 0 })
+  likes: number;
+}
