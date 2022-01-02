12bdf1537af416341c7faaaa3e136d6640961e34
Rakesh D R <rakesh@rapidinnovation.dev>
2022-01-02 08:35:10 +0530

    Added services run scripts

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nest-rabbitmq
+
+Running Services
+
+RabbitMq - docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.9-management
+
+Mysql - docker run --name mysql-container -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -d mysql
+
+MongoDB - docker run --name mongo-container -p 27017:27017 -d mongo
--- main/src/main.ts
+++ main/src/main.ts
@@ -3,21 +3,8 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // app.setGlobalPrefix('/api/v1');
-  // await app.listen(3001);
-
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.RMQ,
-    options: {
-      urls: ['amqp://localhost:5672'],
-      queue: 'main_queue',
-      queueOptions: {
-        durable: false,
-      },
-    },
-  });
-
-  app.listen();
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api/v1');
+  await app.listen(3001);
 }
 bootstrap();
